5ef78bf31b1289fbcc23fb348b6d691bf9c733b5
12bia <alphawolf244@gmail.com>
2023-05-04 15:30:04 -0400

    first commit
    atividade média,media python

---
+++ media.py
@@ -0,0 +1,17 @@
+##exerciocio IMC
+
+nome="bia"
+idade="17"
+peso= 60.00
+altura=1.89
+
+
+imc= peso/altura**2
+
+print(" Seus dados são:\n")
+print("nome:",nome,"\n""idade:",idade,"\n","peso:",peso,"\n""altura:",altura)
+print("seu imc é de:",imc)
+
+
+
+
