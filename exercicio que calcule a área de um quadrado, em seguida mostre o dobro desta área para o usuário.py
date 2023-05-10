e149fdae8caa1152e7c3b8a249e1f0c135bb2225
12bia <alphawolf244@gmail.com>
2023-05-10 17:06:41 -0400

    ##exercicio que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
    ##exercicio que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.

---
+++ exercicio que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.py
@@ -0,0 +1,7 @@
+##exercicio que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado=float(input(" digite o lado do seu quadrado:"))
+
+quadrado=(lado**2) *2
+
+print(" o seu quadrado deu:",quadrado)
