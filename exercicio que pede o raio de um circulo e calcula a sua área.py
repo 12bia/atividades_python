fe60de016914ec1109db5a163aac0b84827a72d5
12bia <alphawolf244@gmail.com>
2023-05-10 16:54:34 -0400

    programa que peça o raio de um circulo, calcule e mostre a sua área.
    programa que peça o raio de um circulo, calcule e mostre a sua área.

---
+++ exercicio que pede o raio de um circulo e calcula a sua área.py
@@ -0,0 +1,11 @@
+
+##faça um programa que peça o raio de um circulo, calcule e mostre a sua área.
+
+raio=float(input(" Digite o valor do raio:"))
+
+area= (raio**2) *3,14
+
+
+## O RAIO DE UM CIRCULO!
+
+print(" a área do seu raio é de:", area)
--- exercicio que pede os dados do aluno e faz a média.py
+++ exercicio que pede os dados do aluno e faz a média.py
@@ -3,9 +3,15 @@
 
 ##  faça um programa que peça o nome,série,escola,cidade e as 4 notas bimestrais e mostre a média.
 
+## entrada
+print(" Bem vindo a escola Broetto!")
+print(" digite os dados abaixo para ver a sua média!")
+
+print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
 
 nome=input(" Digite seu nome:")
-série=int(input(" Digite sua série:"))
+série=input(" Digite sua série:")
 escola=input(" Digite o nome da sua escola:")
 cidade=input(" Digite o nome da sua cidade:")
 
@@ -16,6 +22,7 @@ not3=float(input(" Digite a sua nota  bimestral 3:"))
 not4=float(input(" Digite a sua nota  bimestral 4:"))
 
 media=(not1+not2+not3+not4)/4
+
 
 print(" Olá ",nome," a sua média foi de:",media)
 
