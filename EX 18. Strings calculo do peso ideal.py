51817945e0733caa89e324233c62608b73027e5f
12bia <alphawolf244@gmail.com>
2023-05-12 17:36:37 -0400

    first commit # Tendo como dados de entrada a altura de uma pessoa construa um algoritmo que calcule seu peso idela, usando a seguinte formula: (72.7*altura)-58
    # Tendo como dados de entrada a altura de uma pessoa construa um algoritmo que calcule seu peso idela, usando a seguinte formula: (72.7*altura)-58

--- EX 18. Strings calculo do peso ideal.py
+++ EX 18. Strings calculo do peso ideal.py
@@ -1,3 +1,4 @@
+
 # Tendo como dados de entrada a altura de uma pessoa construa um algoritmo que calcule seu peso idela, usando a seguinte formula: (72.7*altura)-58
 
 altura=float(input(" Digite sua altura:"))
@@ -5,3 +6,21 @@ altura=float(input(" Digite sua altura:"))
 peso_ideal=(72.7*altura)-58
 
 print(" Seu peso ideal seria de:%.2f"% peso_ideal," quilos!")
+
+# Tendo como dados de entrada a altura de uma pessoa construa um algoritmo que calcule seu peso idela, usando a seguinte formula: (72.7*altura)-58
+
+altura=float(input(" Digite sua altura:"))
+
+peso_ideal=(72.7*altura)-58
+
+print(" Seu peso ideal seria de:%.2f"% peso_ideal," quilos!")
+
+
+
+
+
+
+
+
+
+
