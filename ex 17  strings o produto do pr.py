72440bce0ccc2003b68040cc0bc99b5266905a17
12bia <alphawolf244@gmail.com>
2023-05-12 14:17:56 -0400

    first commit  o produto do dobro do primeiro com metade do segundo, a soma do triplo do primeiro com o terceiro, o terceiro elevado ao cubo.
     exercicio 17.strings .o produto do dobro do primeiro com metade do segundo, a soma do triplo do primeiro com o terceiro, o terceiro elevado ao cubo.

---
+++ ex 17  strings o produto do pr.py
@@ -0,0 +1,46 @@
+## o produto do dobro do primeiro com metade do segundo, a soma do triplo do primeiro com o terceiro, o terceiro elevado ao cubo.
+
+n1=float(input(" Digite o primeiro numero:\n"))
+n2=float(input(" Digite o segundo numero:\n"))
+n3=float(input(" Digite o terceiro numero:\n"))
+
+print(" o produto do dobro do primeiro com metade do segundo é de:",(n1*2)*(n2/2))
+print(" a soma do triplo do primeiro com o terceiro:", (n1*3) + n3)
+print(" O terceiro elevado ao cubo:",( n3**3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
