582766021a0f55bac7d9721c744f9a7018f2d9ae
12bia <alphawolf244@gmail.com>
2023-05-17 15:17:08 -0400

    atividade de receber 3 numeros e imprimir o maior deles.

---
+++ faça um programa que leia 3 numeros e mostre o maior deles.py
@@ -0,0 +1,16 @@
+# faça um programa que leia três numeros e mostre o maior deles.
+
+
+n1=float(input(" Digite  o primeiro numero:"))
+n2=float(input(" Digite o segundo numero:"))
+n3=float(input(" digite o terceiro numero"))
+
+if n1>n2:
+    print(n1)
+
+elif n2>n3:
+    print(n2)
+else:
+    n1<n2
+    print(n3)
+
