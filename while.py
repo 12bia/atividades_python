41e9483b4ac17786f57f0e51c5dc7138624aaa71
12bia <alphawolf244@gmail.com>
2023-05-22 17:35:48 -0400

    # Estrutura de repetição #python - Break
    # Estrutura de repetição
    #python - Break

---
+++ while.py
@@ -0,0 +1,35 @@
+
+# Estrutura de repetição
+#python - Break
+'''while True:
+    print(" Loop infinito\n")'''
+
+# Break
+# em python a instrução break oferece a possibilidade de sair de um loop quando uma condição externa é acionada.
+'''while True:
+    valor=int(input(" digite o valor de 1 ou 0 para encerrar!"))
+
+    if valor==1:
+        print(" Valor correto!")
+
+    else: 
+        print(" Valor para sair!")
+
+        break    '''
+
+####################################################################333
+# continue
+
+'''while True:
+    valor =int(input(" Digite o valor de 1 ou 0 para encerrar:"))
+
+    if valor >=1:
+        continue
+    print(" maior que um")
+
+    if valor>=1:
+        print(" Maior que um")# ele vai ficar no laço de repetição até a pessoa digitar 0, e ele vai imprimir maior que um, se digitar 1 ou maior que 1, ele vai continuar pedindo, para digitar.
+'''
+##################################################################################
+
+#
