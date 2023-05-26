b25fe91ff82f2b0f4fd34f9fc0055416468c235d
12bia <alphawolf244@gmail.com>
2023-05-26 14:12:33 -0400

    atividade onde o usuario digita um numero inicial e final e soma eles.
    atividade onde o usuario digita um numero inicial e final e soma eles.

---
+++ altere o programa anterior para mostrar no final a soma dos numeros.py
@@ -0,0 +1,26 @@
+# faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+'''
+ini = int ( input ( "Digite um número inicial:" ))
+fi = int ( input ( "Digite o número final:" ))
+
+for  i  in  range ( ini , fi ):
+    print ( i )'''
+
+
+#altere o programa anterior para mostrar no final a soma dos numeros.
+print(" ☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆")
+
+ini = int ( input ( "Digite um número inicial:" ))
+fi = int ( input ( "Digite o número final:" ))
+
+for  i  in  range ( ini , fi ):
+    print ( i)
+
+    soma=ini+fi
+print(" A soma dos números digitados é de:",soma)
+print(" ☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆☆ ☆ ☆ ☆ ☆ ☆ ☆")
+    
+
+
+    
+
